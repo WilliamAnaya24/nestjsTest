8f83f7d5becf79063e35cb71e8b1e4dde18e07a0
william.anaya <william.anaya@globant.com>
2022-07-14 11:08:28 -0500

    testing create method

--- src/components/organization/organization.controller.spec.ts
+++ src/components/organization/organization.controller.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrganizationController } from './organization.controller';
 import { OrganizationService } from './organization.service';
+import { MockService } from "../mock/mock.service";
 
 describe('OrganizationController', () => {
   let controller: OrganizationController;
+  const mockOrganizationService = {
+    create: jest.fn((dto) => {
+      return {
+        id: 1,
+        ...dto,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrganizationController],
       providers: [OrganizationService],
-    }).compile();
+    })
+      .overrideProvider(OrganizationService)
+      .useValue(mockOrganizationService)
+      .compile();
 
     controller = module.get<OrganizationController>(OrganizationController);
   });
@@ -18,9 +30,17 @@ describe('OrganizationController', () => {
     expect(controller).toBeDefined();
   });
 
-  describe('Should create a new Organization', () => {
-    it('Should create a new Organization with data obtained', () => {
-
-    })
-  })
+  it('should create a organization', () => {
+    const dto = {
+      name: 'test',
+      status: 1,
+      id: 0,
+    };
+    expect(controller.create(dto)).toEqual({
+      id: expect.any(Number),
+      name: 'test',
+      status: 1,
+    });
+    expect(mockOrganizationService.create).toHaveBeenCalledWith(dto);
+  });
 });
